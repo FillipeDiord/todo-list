f15ed3941ccdcacbad3af43d6a88afaccb2bab88
Fillipe Diord <fillipe.diord@hotmail.com>
2023-08-25 13:16:46 -0300

    rename variable

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ function addTodoList() {
 
     formAddTodo.addEventListener('submit', (event) => {
         event.preventDefault();
-        const inputValue = formAddTodo.add.value.trim();
+        const nameTodo = formAddTodo.add.value.trim();
 
-        if (inputValue) {
+        if (nameTodo) {
             const list = document.querySelector('.todos-container');
 
             list.innerHTML +=
                 `<li class="list-group-item d-flex justify-content-between align-items-center">
-                    <span>${inputValue}</span>
+                    <span>${nameTodo}</span>
                     <i class="far fa-trash-alt delete"></i>
                 </li>`;
 
