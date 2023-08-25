8897adfa0652181bce3ce39b2b8a110fd30f599d
Fillipe Diord <fillipe.diord@hotmail.com>
2023-08-25 12:01:43 -0300

    adding function for adding todos in list

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+function addTodoList() {
+    const formAddTodo = document.querySelector('.form-add-todo');
 
+    formAddTodo.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const inputValue = formAddTodo.add.value.trim();
+
+        if (inputValue) {
+            const list = document.querySelector('.todos-container');
+
+            list.innerHTML +=
+                `<li class="list-group-item d-flex justify-content-between align-items-center">
+                    <span>${inputValue}</span>
+                    <i class="far fa-trash-alt delete"></i>
+                </li>`;
+
+            event.target.reset();
+        };
+    })
+}
+
+addTodoList();
